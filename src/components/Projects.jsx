637de6f7cb80a2b97cdcefee772d6be9ac46e833
KRISHNA KUMAR MANDAL <kkmandal2030@gmail.com>
2025-02-10 20:46:45 +0530

    project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ const Project = () => {
   return (
     <div className="border-b border-gray-300 pb-4">
       <h1 className="my-20 text-center text-4xl">Project</h1>
-      <div>
+      {/* <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center ">
             <div className="w-full lg:w-1/4">
@@ -30,6 +30,10 @@ const Project = () => {
             </div>
           </div>
         ))}
+      </div> */}
+
+      <div>
+        <h1>krishna</h1>
       </div>
     </div>
   );
